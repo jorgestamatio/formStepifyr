cc7577ccbe2027e26d789700e38d124339fb8919
Jorge Stamatio <jorge.stamatio@gmail.com>
2013-12-13 00:29:53 +0100

    Added support for multiple instances of the plugin

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # formStepifyr
 
-## A simple jQuery plugin to convert a long html form into a series of steps.
+A simple jQuery plugin to convert a long html form into a series of steps.
 
 ### Usage
 
 Include formStepifyr.js
+
+If you wish to add CSS trasitions you can include animate.css [https://github.com/daneden/animate.css] or you can declare your own animations.
+
 
 #### HTML
 
@@ -39,13 +42,16 @@ Include formStepifyr.js
 
 Call it on any form element like this:
 
-    $('#myForm').formStepifyr({submitButton: 'formSubmit'});
+    $('#myForm').formStepifyr();
 
 
 For more options
 
     $('#myForm').formStepifyr({
-      submitButton: 'formSubmit', 
-      animationPrev: 'fadeInLeft',
-      animationNext: 'fadeInRight'
+      submitButton: 'formSubmit',  // lets you specify the id of the submit. useful if you are using other than a input[type="submit"] 
+      animationPrev: 'fadeInLeft', // adds this class to the prev element on show in case you want to use a CSS animation. 
+      animationNext: 'fadeInRight', // adds this class to the next element on show in case you want to use a CSS animation. 
+      stepCounter: false,  // show how many steps are there. (default: false).
+      btnClassPrev: "",  // optional classes for the prev button (btn btn-default).
+      btnClassNext: "" // optional classes for the prev button (btn btn-success).
     });
--- formStepifyr.js
+++ formStepifyr.js
@@ -1,82 +1,141 @@
-/* Based on the formToWizard plugin by jankoatwarpspeed.com , modified to work better with CSS animations and Twitter Bootstrap*/
+/* 
+// Based on the formToWizard plugin by jankoatwarpspeed.com (kudos to him!). 
+// modified to work better with CSS animations and Twitter Bootstrap
+// Also added support for multiple instances of the plugin.
+*/
 
 (function($) {
     $.fn.formStepifyr = function(options) {
-        options = $.extend({  
+        options = $.extend({
+            stepCounter: false,  
             submitButton: "",
-            animationNext: "fadeInRight",
-            animationPrev: "fadeInLeft"
+            animationNext: "",
+            animationPrev: "",
+            btnClassPrev: "",
+            btnClassNext: ""
         }, options); 
         
         var element = this;
+        var elementId = $(element).attr("id");
 
         var steps = $(element).find("fieldset");
         var count = steps.size();
-        var submmitButtonName = "#" + options.submitButton;
-        $(submmitButtonName).hide();
 
-        // 2
-        $(element).before("<ul id='steps' class='pagination'></ul>");
+        if(options.submitButton != ""){
+          var submitBtn = "#" + options.submitButton;
+          console.log('using name');
+        }else{
+          var submitBtn = $(element).find("input[type='submit']");
+          console.log('using input');
+        }
+
+        $(submitBtn).hide();
+
+        if(options.stepCounter){
+          $(element).before("<ul id='steps"+ elementId +"' class='pagination'></ul>");
+        } 
+        
 
         steps.each(function(i) {
-            $(this).wrap("<div id='step" + i + "' class='animated'></div>");
-            $(this).append("<p id='step" + i + "commands'></p>");
+            $(this).wrap("<div id='step" + elementId + i + "' class='step animated'></div>");
+            $(this).append("<p id='step" + elementId + i + "commands'></p>");
 
-            // 2
-            var name = $(this).find("legend").html();
-            // $("#steps").append("<li id='stepDesc" + i + "'>Step " + (i + 1) + "<span>" + name + "</span></li>");
-            $("#steps").append("<li id='stepDesc" + i + "' class='disabled'><span>" + (i + 1) + "</span></li>");
+            
+            if(options.stepCounter){
+              var name = $(this).find("legend").html();
+              $("#steps" + elementId).append("<li id='stepDesc"+ elementId + i + "' class='disabled'><span>" + (i + 1) + "</span></li>");
+            }
+
 
             if (i == 0) {
                 createNextButton(i);
                 selectStep(i);
             }
             else if (i == count - 1) {
-                $("#step" + i).hide();
+                $("#step" + elementId + i).hide();
                 createPrevButton(i);
             }
             else {
-                $("#step" + i).hide();
+                $("#step" + elementId + i).hide();
                 createPrevButton(i);
                 createNextButton(i);
             }
         });
 
         function createPrevButton(i) {
-            var stepName = "step" + i;
-            $("#" + stepName + "commands").append("<a href='#' id='" + stepName + "Prev' class='prev btn btn-default'>< Back</a>");
+            var stepName = "step" + elementId + i;
+            $("#" + stepName + "commands").append("<a href='#' id='" + stepName + "Prev' class='prev " + options.btnClassPrev + "'>< Back</a>");
 
             $("#" + stepName + "Prev").bind("click", function(e) {
+
                 e.preventDefault();
-                //$("#" + stepName).removeClass("fadeInRight").removeClass("fadeInLeft");
-                $("#" + stepName).removeClass(options.animationPrev).removeClass(options.animationNext);
                 $("#" + stepName).hide();
-                $("#step" + (i - 1)).show();
-                $("#step" + (i - 1)).addClass(options.animationPrev);
-                $(submmitButtonName).hide();
-                selectStep(i - 1);
+                $("#step" + elementId + (i - 1)).show();
+
+                if(options.animationPrev != ''){
+                   $("#" + stepName).removeClass(options.animationPrev).removeClass(options.animationNext);
+                   $("#step" + elementId + (i - 1)).addClass(options.animationPrev);
+                }
+
+                $(submitBtn).hide();
+
+                if(options.stepCounter){
+                  selectStep(i - 1);
+                }
             });
+
+
         }
 
         function createNextButton(i) {
-            var stepName = "step" + i;
-            $("#" + stepName + "commands").append("<a href='#' id='" + stepName + "Next' class='next btn btn-default pull-right'>Next ></a>");
+          var stepName = "step" + elementId + i;
+          $("#" + stepName + "commands").append("<a href='#' id='" + stepName + "Next' class='next " + options.btnClassNext + "'>Next ></a>");
 
-            $("#" + stepName + "Next").bind("click", function(e) {
-                e.preventDefault();
-                $("#" + stepName).removeClass(options.animationPrev).removeClass(options.animationNext);
-                $("#" + stepName).hide();
-                $("#step" + (i + 1)).show();
-                $("#step" + (i + 1)).addClass(options.animationNext);
-                if (i + 2 == count)
-                    $(submmitButtonName).show();
-                selectStep(i + 1);
-            });
+          $("#" + stepName + "Next").bind("click", function(e) {
+              e.preventDefault();
+              var noError = true;
+
+                $("#" + stepName).find('.form-control').each(function(){
+                  if($( this ).val() == ''){
+                    $( this ).parent(".form-group").addClass('has-error');
+                    noError = false;
+                  }
+                });
+
+
+                $("#" + stepName).find(".form-control").on('change blur',function(){
+                  if($( this ).parent(".form-group").addClass('has-error')){
+                    if($(this).val != ''){
+                      $( this ).parent(".form-group").removeClass('has-error');
+                    }
+                  }
+                });
+
+                if(noError == true){
+                  $("#" + stepName).hide();
+                  $("#step" + elementId + (i + 1)).show();
+
+                  if(options.animationPrev != '' && options.animationNext != ''){
+                    $("#" + stepName).removeClass(options.animationPrev).removeClass(options.animationNext);
+                    $("#step" + elementId + (i + 1)).addClass(options.animationNext);
+                  }
+
+                  if (i + 2 == count){
+                    $(submitBtn).show();
+                  }
+
+                  if(options.stepCounter){
+                    selectStep(i + 1);
+                  }
+                }
+              
+
+          });
         }
 
         function selectStep(i) {
-            $("#steps li").removeClass("active");
-            $("#stepDesc" + i).addClass("active");
+          $("#steps"+ elementId+" li").removeClass("active");
+          $("#stepDesc"+ elementId + i).addClass("active");
         }
 
     }
